f5edc1481a8a65b7cee46ec82db80125728c24a0
Andy Ching <dogdoglization@gmail.com>
2019-07-23 20:29:27 +0800

    update topbar effect
    update topbar effect

--- assets/js/topbar-actions.js
+++ assets/js/topbar-actions.js
@@ -1,10 +1,45 @@
 "use strict";
 
-(function(d) {
-    const menuBtn = d.getElementsByClassName("app-topbar-menu-btn")[0];
-    const sidebar = d.getElementsByClassName("app-header")[0];
+(function(d, w) {
+    /**common tools */
+    function eleByClass(className) { return d.getElementsByClassName(className)[0] }
+    function hasClass(ele, className) { return ele.className.indexOf(className) > -1; }
+    function addClass(ele, className) { ele.className += " " + className; }
+    function delClass(ele, className) { ele.className = ele.className.replace(className, "").trim(); }
+
+    /** menu btn action */
+    const menuBtn = eleByClass("app-topbar-menu-btn");
+    const sidebar = eleByClass("app-header");
     menuBtn.onclick = function() {
-        const className = sidebar.className;
-        sidebar.className = className.indexOf("show") > -1 ? className.replace("show", "").trim() : (className + " show");
+        hasClass(sidebar, "show") ? delClass(sidebar, "show") : addClass(sidebar, "show");
+    };
+
+    /** top bar scrolling effect */
+    let isScrolling = false;
+    let isScrollingTimer = null;
+    let lastScroll = 0;
+    const topbar = eleByClass("app-topbar");
+    function onScrolling() {
+        isScrolling = true;
+        const diff = lastScroll - w.scrollY;
+        if (diff < 0 && !hasClass(topbar, "hide")) {
+            addClass(topbar, "hide");
+        }
+        else if (diff > 0 && hasClass(topbar, "hide")) {
+            delClass(topbar, "hide");
+        }
     }
-})(document);
+    function offScrolling() {
+        isScrolling = false;
+        lastScroll = w.scrollY;
+    }
+    w.addEventListener("scroll", function() {
+        if (!isScrolling) {
+            onScrolling();
+        }
+        if (isScrollingTimer) {
+            clearTimeout(isScrollingTimer);
+        }
+        isScrollingTimer = setTimeout(offScrolling, 300);
+    });
+})(document, window);
